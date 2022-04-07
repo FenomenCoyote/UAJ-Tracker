1196b590084edc0e569d0d99284139045d76c9f2
FenomenCoyote <javcruz@ucm.es>
2022-04-07 17:24:59 +0200

    Tracker Singleton
    Co-Authored-By: nicoFdez <47755232+nicoFdez@users.noreply.github.com>

--- TrackerSystem/Source/TrackerSystem/Tracker.cpp
+++ TrackerSystem/Source/TrackerSystem/Tracker.cpp
@@ -1,2 +1,77 @@
 #include "pch.h"
-#include "Test.h"
+#include "Tracker.h"
+#include "TrackerEvent.h"
+#include "IPersistance.h"
+
+#include "SessionStartEvent.h"
+#include "SessionEndEvent.h"
+
+
+Tracker* Tracker::instance = nullptr;
+
+
+Tracker::Tracker(): gameID(), sessionID()
+{
+
+}
+
+Tracker::~Tracker()
+{
+}
+
+
+bool Tracker::Init()
+{
+    instance = new Tracker();
+
+    return true;
+}
+
+void Tracker::setUserID(const std::string& userNameID)
+{
+    /*
+        
+    string cmd = "getmac";
+    string filename = "macaddress.txt";
+
+    system((cmd + ">" + filename).c_str());
+    string line;
+    ifstream myfile("macaddress.txt");
+
+    if (myfile.is_open()) {
+        while (getline(myfile, line)) {
+            cout << line << endl;
+        }
+        myfile.close();
+    }
+    else
+        cout << "Unable to open the file";
+    
+    */
+
+    userID = userNameID;
+}
+
+void Tracker::setStoragePath(const std::string& path)
+{
+    this->path = path;
+}
+
+bool Tracker::Start()
+{
+    trackEvent<SessionStartEvent>();
+
+    return true;
+}
+
+bool Tracker::End()
+{
+    trackEvent<SessionEndEvent>();
+
+    return true;
+}
+
+std::time_t Tracker::getTimestamp()
+{
+    return std::time(nullptr);
+}
--- TrackerSystem/Source/TrackerSystem/Tracker.h
+++ TrackerSystem/Source/TrackerSystem/Tracker.h
@@ -1,9 +1,100 @@
 #pragma once
 #include <iostream>
-class Test
+#include <cassert>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <queue>
+#include <array>
+
+
+class IPersistance;
+class TrackerEvent;
+
+class Tracker
 {
+private:
+	static Tracker* instance;
 public:
 
-	static void hola() { std::cout << "hola"; }
+	/**
+	 * Inits system
+	 * @return true when all good
+	 * @return false when something bad happened
+	 */
+	static bool Init();
+
+	inline static Tracker* Instance() {
+		assert(instance != nullptr);
+		return instance;
+	}
+
+	/**
+	 * 
+	 * @param userNameID 
+	 */
+	void setUserID(const std::string& userNameID);
+
+	/**
+	 * Game ID 
+	 * @param gameID int
+	 */
+	void setGameID(const int gameID) { this->gameID = gameID; }
+
+	/**
+	 * Where to store data
+	 * @param path 
+	 */
+	void setStoragePath(const std::string& path);
+
+	/**
+	 * Start of session
+	 * @return true 
+	 * @return false 
+	 */
+	bool Start();
+
+	/**
+	 * End of session
+	 * @return true 
+	 * @return false 
+	 */
+	bool End();
+
+	template<typename T = TrackerEvent, typename ...Targs>
+	void trackEvent(Targs&&... args) {
+		TrackerEvent* te = new T(std::forward<Targs>(args)...);
+
+		if (te != nullptr) {
+			te->setTimeStamp(getTimestamp());
+			te->setGameId(gameID);
+			te->setSessionId(sessionID);
+
+			//TODO: string OR unsigned long int
+			te->setUserId(userID);
+		}
+
+		persistance->send(te);
+	}
+
+private:
+
+	Tracker(); 
+
+	~Tracker(); 
+	
+	/**
+	* time_t == long long
+	*/
+	std::time_t getTimestamp();
+
+	std::string path;
+
+	std::string userID;
+	
+	int gameID;
+	int sessionID;
+
+	IPersistance* persistance;
 };
 
