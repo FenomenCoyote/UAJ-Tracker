7f094420d918be3a9c98e18c9efff54d697b509e
MarcoIMH <47715985+MarcoIMH@users.noreply.github.com>
2022-04-07 12:49:59 +0200

    AnimalCooking Events
    Co-Authored-By: Alonefcp <47754529+Alonefcp@users.noreply.github.com>

--- TrackerSystem/Source/TrackerSystem/TrackerEvent.cpp
+++ TrackerSystem/Source/TrackerSystem/TrackerEvent.cpp
@@ -1,2 +1,65 @@
 #include "pch.h"
-#include "Test.h"
+#include "TrackerEvent.h"
+
+TrackerEvent::TrackerEvent()
+{
+}
+
+TrackerEvent::~TrackerEvent()
+{
+}
+
+bool TrackerEvent::toJson()
+{
+}
+
+int TrackerEvent::getTimeStamp() const
+{
+	return _timeStamp;
+}
+
+unsigned long int TrackerEvent::getUserId() const
+{
+	return _userId;
+}
+
+unsigned short int TrackerEvent::getSessionId() const
+{
+	return _sessionId;
+}
+
+uint8_t TrackerEvent::getGameId() const
+{
+	return _gameId;
+}
+
+uint16_t TrackerEvent::getEventId() const
+{
+	return _eventId;
+}
+
+
+void TrackerEvent::setTimeStamp(int tS)
+{
+	_timeStamp = tS;
+}
+
+void TrackerEvent::setUserId(unsigned long int uI)
+{
+	_userId = uI;
+}
+
+void TrackerEvent::setSessionId(unsigned short int sI)
+{
+	_sessionId = sI;
+}
+
+void TrackerEvent::setGameId(uint8_t gI)
+{
+	_gameId = gI;
+}
+
+void TrackerEvent::setEventId(uint16_t eI)
+{
+	_eventId = eI;
+}
--- TrackerSystem/Source/TrackerSystem/TrackerEvent.h
+++ TrackerSystem/Source/TrackerSystem/TrackerEvent.h
@@ -1,9 +1,41 @@
 #pragma once
 #include <iostream>
-class Test
-{
+#include <chrono>
+
+/* 
+userId: 
+	- 2 bytes: user code
+	- 6 bytes: mac code (12 hexadecimal - 4 bits per hexadecimal)
+
+sessionId:	
+	- 2 bytes: 65.500 sessions aprox (1 time every day during 178 years) SessionId:
+	- 2 bytes: 65.500 sessions aprox (1 time every day during 178 years)
+
+gameId:
+	- 1 byte: 255 differents games
+*/
+
+class TrackerEvent {
 public:
+	TrackerEvent();
+	~TrackerEvent();
+	bool toJson();
+	
+	inline int getTimeStamp() const;
+	inline unsigned long int getUserId() const;
+	inline unsigned short int getSessionId() const;
+	inline uint8_t getGameId() const;
+	inline uint16_t getEventId() const;
 
-	static void hola() { std::cout << "hola"; }
+	inline void setTimeStamp(int tS);
+	inline void setUserId(unsigned long int uI);
+	inline void setSessionId(unsigned short int sI);
+	inline void setGameId(uint8_t gI);
+	inline void setEventId(uint16_t eI);
+private:
+	int _timeStamp;
+	unsigned long int _userId;
+	unsigned short int _sessionId;
+	uint8_t _gameId;
+	uint16_t _eventId;
 };
-
